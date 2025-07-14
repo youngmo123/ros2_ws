19a22de507702dbb647ce21f043fe23a2a7bca22
youngmo123 <youngmo123@hanmail.net>
2025-07-14 14:18:49 +0900

    주석

--- src/ros_study_py/ros_study_py/my_service_server.py
+++ src/ros_study_py/ros_study_py/my_service_server.py
@@ -5,11 +5,13 @@ from ros_study_msgs.srv import MySrv
 class MyServiceServer(Node):
     def __init__(self):
         super().__init__('my_service_server')
-        # 서비스 생성
+        # 노드 이름 정의
         self.srv = self.create_service(MySrv, 'my_service', self.callback)
+        # 서비스서버 만드는 함수 서비스 이름도 정의
         self.get_logger().info('Service server ready')
 
     def callback(self, request, response):
+        #request, response를 어떻게 주고 받냐?
         self.get_logger().info(f'Received request: num={request.num}')
         response.res = request.num * 2.0  # 예: 두 배로 반환
         return response
