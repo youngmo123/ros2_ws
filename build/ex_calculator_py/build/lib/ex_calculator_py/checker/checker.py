8ac3417f491ea8075b2e890ad4e7b19db97a30c6
youngmo123 <youngmo123@hanmail.net>
2025-07-15 14:21:28 +0900

    checker

---
+++ build/ex_calculator_py/build/lib/ex_calculator_py/checker/checker.py
@@ -0,0 +1,55 @@
+from action_msgs.msg import GoalStatus
+from ros_study_msgs.action import ArthmeticChecker
+from rclpy.action import ActionClient
+from rclpy.node import Node
+
+
+class Checker(Node):
+
+    def __init__(self):
+        super().__init__('checker')
+        self.arithmetic_action_client = ActionClient(
+          self,
+          ArthmeticChecker,
+          'arithmetic_checker')
+
+    def send_goal_total_sum(self, goal_sum):
+        wait_count = 1
+        while not self.arithmetic_action_client.wait_for_server(timeout_sec=0.1):
+            if wait_count > 3:
+                self.get_logger().warning('Arithmetic action server is not available.')
+                return False
+            wait_count += 1
+        goal_msg = ArthmeticChecker.Goal()
+        goal_msg.goal_sum = (float)(goal_sum)
+        self.send_goal_future = self.arithmetic_action_client.send_goal_async(
+            goal_msg,
+            feedback_callback=self.get_arithmetic_action_feedback)
+        self.send_goal_future.add_done_callback(self.get_arithmetic_action_goal)
+        return True
+
+    def get_arithmetic_action_goal(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().warning('Action goal rejected.')
+            return
+        self.get_logger().info('Action goal accepted.')
+        self.action_result_future = goal_handle.get_result_async()
+        self.action_result_future.add_done_callback(self.get_arithmetic_action_result)
+
+    def get_arithmetic_action_feedback(self, feedback_msg):
+        action_feedback = feedback_msg.feedback.formula
+        self.get_logger().info('Action feedback: {0}'.format(action_feedback))
+
+    def get_arithmetic_action_result(self, future):
+        action_status = future.result().status
+        action_result = future.result().result
+        if action_status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info('Action succeeded!')
+            self.get_logger().info(
+                'Action result(all formula): {0}'.format(action_result.all_formula))
+            self.get_logger().info(
+                'Action result(total sum): {0}'.format(action_result.total_sum))
+        else:
+            self.get_logger().warning(
+                'Action failed with status: {0}'.format(action_status))
