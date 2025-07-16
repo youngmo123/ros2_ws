8696706f1e9f50937896f221cf5532c1682a15c8
youngmo123 <youngmo123@hanmail.net>
2025-07-16 13:42:05 +0900

    rebuild2

--- install/cpp_excalculator/share/cpp_excalculator/launch/arithmetic.launch.py
+++ install/cpp_excalculator/share/cpp_excalculator/launch/arithmetic.launch.py
@@ -20,6 +20,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch_ros.actions import PushRosNamespace
 
 
 def generate_launch_description():
@@ -29,8 +30,11 @@ def generate_launch_description():
             get_package_share_directory('cpp_excalculator'),
             'param',
             'arithmetic_config.yaml'))
-
+    
     return LaunchDescription([
+           PushRosNamespace(
+            namespace='bym'
+        ),
         DeclareLaunchArgument(
             'param_dir',
             default_value=param_dir,
